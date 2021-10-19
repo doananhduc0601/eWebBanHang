b9b3e5e445c487505d293cf2948d95a6a0e0059e
Đoàn Anh Đức <ducpda111@gmail.com>
2021-10-19 08:37:56 +0700

    Add project files.

---
+++ ShopWebBanHang/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ShopWebBanHang
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
